1666d6418f7083faddc2904af60194851e5e0532
jbuendia19 <jezreelbuendia@gmail.com>
2014-06-25 21:47:04 +0100

    task 12

--- field_class.py
+++ field_class.py
@@ -3,7 +3,7 @@ from wheat_class import *
 from sheep_class import *
 from cow_class import *
 
-class Field():
+class Field:
     """Simulate a field that can contain animals and crops"""
 
     def __init__(self,max_animals,max_crops):
@@ -32,12 +32,21 @@ class Field():
     def remove_animal(self,position):
         return self._animals.pop(position)
 
+    def report_contents(self):
+        crop_report = []
+        animal_report = []
+        for crop in self._crops:
+            crop_report.append(crop.report())
+        for animal in self._animals:
+            animal_report.append(animal.report())
+        return {"crops": crop_report, "animals":animal_report}
+
 def display_crops(crop_list):
     print()
     print("The following crops are in this field: ")
     pos = 1
     for crop in crop_list:
-        print("{0:>2} .{1}".format(pos,crop.report()))
+        print("{0:>2}. {1}".format(pos,crop.report()))
         pos+= 1
 
 def display_animals(animal_list):
@@ -47,15 +56,50 @@ def display_animals(animal_list):
     for animal in animal_list:
         print("{0:>2}. {1}".format(pos,animal.report()))
         pos+= 1
-        
+
+def select_crop(length_list):
+    valid = False
+    while not valid:
+        selected = int(input("Please select a crop: "))
+        if selected in range(1,length_list+1):
+            valid = True
+        else:
+            print("Please select a valid option")
+    return selected - 1
+
+def select_animal(length_list):
+    valid = False
+    while not valid:
+        selected = int(input("PLease select an animal: "))
+        if selected in range(1,length_list+1):
+            valid = True
+        else:
+            print("Please enter a valid option")
+    return selected
+
+def harvest_crop_from_field(field):
+    display_crops(field._crops)
+    selected_crops = select_crop(len(field._crops))
+    return field.harvest_crop(selected_crops)
+
+def remove_animal_from_field(field):
+    display_animals(field._animals)
+    slected_animal = select_animal(len(field._animals))
+    return field.remove_animal(selected_animal)
+                    
+
+     
 def main():
     new_field = Field(5,2)
     new_field.plant_crop(Wheat())
     new_field.plant_crop(Potato())
     new_field.add_animal(Sheep)
     new_field.add_animal(Cow)
-    display_crops(new_field._crops)
-    display_animals(new_field._animals)
+    report = new_field.report_contents()
+    print(report["animals"])
+    print()
+    print(report["crops"])
+    
 
 
 if __name__ == "__main__":
